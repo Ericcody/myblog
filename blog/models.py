edc0401be663f22abc974fdd7e1bc1eea37f892d
pyvip <pyvip@Vip.tz.cn>
2018-06-24 19:19:33 +0800

    first commit

---
+++ blog/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.urls import reverse
+from django.utils.six import python_2_unicode_compatible
+from django.contrib.auth.models import User#内置的应用处理用户的登陆注册。
+import markdown
+from django.utils.html import strip_tags
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200,blank=True)
+    category = models.ForeignKey(Category)# 分类与文章是一对多关系，采用外键
+    tags = models.ManyToManyField(Tag,blank=True)#标签与文章是多对多关系
+    author = models.ForeignKey(User)
+    views = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
+
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+
+    def save(self,*args,**kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(
+                extensions=[
+                    'markdown.extensions.extra',
+                    'markdown.extensions.codehilite',
+                ]
+            )
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post,self).save(*args,**kwargs)
+
+    class Meta:
+        ordering =['-created_time']
